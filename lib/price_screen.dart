d465e6278563897221d25422f6dcb0d3fb0b1060
Shubham Kumar <51149063+ZippySphinx@users.noreply.github.com>
2020-03-12 14:47:49 +0530

    Update price_screen.dart

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
 import 'dart:io' show Platform;
 
@@ -9,53 +9,59 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String selectedCurrency = "AUD";
+  String selectedCurrency = 'AUD';
 
-  DropdownButton<String> getDropdownButton() {
-    List<DropdownMenuItem<String>> dropDownItems = [];
-    for (int i = 0; i < currenciesList.length; i++) {
-      String currency = currenciesList[i];
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
       var newItem = DropdownMenuItem(
         child: Text(currency),
         value: currency,
       );
-      dropDownItems.add(newItem);
+      dropdownItems.add(newItem);
     }
+
     return DropdownButton<String>(
       value: selectedCurrency,
-      items: dropDownItems,
+      items: dropdownItems,
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getData();
         });
       },
     );
   }
 
   CupertinoPicker iOSPicker() {
-    List<Widget> pickerItems = [];
+    List<Text> pickerItems = [];
     for (String currency in currenciesList) {
       pickerItems.add(Text(currency));
     }
+
     return CupertinoPicker(
-        backgroundColor: Colors.deepPurpleAccent,
-        itemExtent: 39,
-        onSelectedItemChanged: (selectedIndex) {
-          print(selectedIndex);
-          setState(() {
-            selectedCurrency = currenciesList[selectedIndex];
-            getData();
-          });
-        },
-        children: pickerItems);
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+          getData();
+        });
+      },
+      children: pickerItems,
+    );
   }
 
-  String bitcoinValue = '?';
+  String value = '?';
+
+  //TODO 7: Figure out a way of displaying a '?' on screen while we're waiting for the price data to come back. Hint: You'll need a ternary operator.
+
+  //TODO 6: Update this method to receive a Map containing the crypto:price key value pairs. Then use that map to update the CryptoCards.
   void getData() async {
     try {
-      var data = await CoinData().getCoinData(selectedCurrency);
+      double data = await CoinData().getCoinData(selectedCurrency);
       setState(() {
-        bitcoinValue = data;
+        value = data.toStringAsFixed(0);
       });
     } catch (e) {
       print(e);
@@ -63,27 +69,30 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  //TODO: For bonus points, create a method that loops through the cryptoList and generates a CryptoCard for each.
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 13,
-        centerTitle: true,
-        title: Text(
-          'Coin Ticker',
-          style: TextStyle(
-            fontSize: 21,
-            fontStyle: FontStyle.italic,
-          ),
-        ),
+        title: Text('🤑 Coin Ticker'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+          //TODO 1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
+          //TODO 2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
+          //TODO 3: You'll need to use a Column Widget to contain the three CryptoCards.
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
-              color: Colors.deepPurpleAccent,
+              color: Colors.lightBlueAccent,
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
@@ -91,7 +100,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = $bitcoinValue $selectedCurrency',
+                  '1 BTC = $value $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -105,8 +114,8 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.deepPurpleAccent,
-            child: iOSPicker(),
+            color: Colors.lightBlue,
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
